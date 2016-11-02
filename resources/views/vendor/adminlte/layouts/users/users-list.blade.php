f156471b76ab2af612b5eebbba41e6f0db40aeaa
Andrés Matta Morales <Andres.sm123@gmail.com>
2016-11-02 11:51:55 -0600

    Vistas Usuarios

---
+++ resources/views/vendor/adminlte/layouts/users/users-list.blade.php
@@ -0,0 +1,30 @@
+<div class="box">
+	<div class="box-header with-border">
+		<h3 class="box-title">Usuarios</h3>
+	</div>
+	<div class="box-body">
+		<table class="table table-bordered">
+			<tbody>
+				<tr>
+					<th>Nombre de Usuario</th>
+					<th>Nombre Completo</th>
+					<th>Tipo de Usuario</th>
+					<th>Última actividad</th>
+					<th>Operaciones</th>
+				</tr>
+				<tr>
+					<th>amatta</th>
+					<th>Andrés Matta Morales</th>
+					<th>Administrador</th>
+					<th><small>12:45, Jueves 12/08/2016</small></th>
+					<th><a href="#" class="btn btn-app">
+						<i class="fa fa-trash-o"></i>
+					</a></th>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<div class="box-footer clearfix">
+		<ul class="pagination pagination-sm no-margin pull-right"></ul>
+	</div>
+</div>
---
+++ resources/views/vendor/adminlte/layouts/users/users.blade.php
@@ -0,0 +1,16 @@
+@extends('adminlte::layouts.app')
+
+@section('htmlheader_title')
+	{{ trans('adminlte_lang::message.home') }}
+@endsection
+
+
+@section('main-content')
+
+@include('user-list')
+
+@endsection
+
+		
+          
+        
